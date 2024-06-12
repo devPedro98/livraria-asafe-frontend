2cb21aeac020ebe21e05ea9663aae2084c8d09ae
pbarrios <pbarrios@magnasistemas.com.br>
2024-06-12 11:13:32 -0300

    paragros removidos de book page, botao toast para teste removido

--- src/pages/BooksPage.jsx
+++ src/pages/BooksPage.jsx
@@ -7,9 +7,6 @@ const BooksPage = () => {
         <div className={styles.books_page}>
             <Header />
             <h1>Bem-vindo à Livraria Asafe</h1>
-            <p>
-                A Livraria Asafe é mais do que uma simples livraria online. Nosso sistema foi criado para oferecer a melhor experiência de leitura para todos os amantes de livros. Aqui, você encontrará uma vasta coleção de títulos, desde clássicos atemporais até os lançamentos mais recentes.
-            </p>
             <Books />
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,10 @@
 import Header from './Header'
 import { Link } from 'react-router-dom'
 import styles from './Home.module.css'
-import { Zoom, toast } from 'react-toastify'
 
 const Home = () => {
     return (
         <div className={styles.home}>
-            <button onClick={() => toast.error("erro rsrs", {
-                autoClose: 3000,
-                transition: Zoom
-            })}>Clique aqui</button>
             <Header />
             <p>Seja bem vindo a Livraria Asafe</p>
             <Link to="/books">
